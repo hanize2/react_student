6f98710483f7cd1cebe3859783d2fa59452cf2e0
JinWooJung <hanize2@gmail.com>
2023-04-23 18:17:52 +0900

    수정

--- css_example/src/components/Sample.jsx
+++ css_example/src/components/Sample.jsx
@@ -1,8 +1,8 @@
 export const Sample = () => {
-    return (
-        // btn btn-primary  class들은 bootstrap에서 제공하는 class 입니다. 공식사이트 문서를 참고하세요.
-        <div>
-            <button class="btn btn-primary">버튼</button>
-        </div>
-    );
+  return (
+    // btn btn-primary  class들은 bootstrap에서 제공하는 class 입니다. 공식사이트 문서를 참고하세요.
+    <div>
+      <button className="btn btn-primary">버튼</button>
+    </div>
+  );
 };
