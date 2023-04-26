ff847deb731e85dee7caf35066d25b14e19957df
JinWooJung <hanize2@gmail.com>
2023-04-27 02:39:42 +0900

    수정

--- ch_07/src/index.js
+++ ch_07/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { AdminFlagProvider } from "./components/providers/AdminFlagProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Context의 값을 참조할 컴포넌트를 Provider로 감싸준다. 대부분 루트 컴포너트이다. */}
+    <AdminFlagProvider>
+      <App />
+    </AdminFlagProvider>
   </React.StrictMode>
 );
