320f115b062fe2170cbc05b8fd8fed0b678e60e2
JinWooJung <hanize2@gmail.com>
2023-04-29 01:26:32 +0900

    수정

--- ch_07/src/App.jsx
+++ ch_07/src/App.jsx
@@ -14,7 +14,7 @@ export const App = () => {
     <div>
       {isAdmin ? <span>관리자입니다</span> : <span>관리자가 아닙니다</span>}
       <button onClick={onClickSwitch}>전환</button>
-      <Card isAdmin={isAdmin} />
+      <Card />
     </div>
   );
 };
