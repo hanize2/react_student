527f03ba6da3dfe2823277439ad7690c82e1655e
JinWooJung <hanize2@gmail.com>
2023-04-29 01:56:12 +0900

    글로벌 state관리 추가 내용

--- ch_07_1/src/App.jsx
+++ ch_07_1/src/App.jsx
@@ -1,20 +1,22 @@
-import { useContext } from "react";
-
-import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
+import { createContext, useState } from "react";
 import { Card } from "./components/Card";
 
+export const AdminFlagContext = createContext({});
+
 export const App = () => {
-  // Context 안의 isAdmin과 업데이트 함수를 얻는다
-  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
+  // 관리자 플래그
+  const [isAdmin, setIsAdmin] = useState(false);
 
   // 전환 클릭 시
   const onClickSwitch = () => setIsAdmin(!isAdmin);
 
   return (
-    <div>
-      {isAdmin ? <span>관리자입니다</span> : <span>관리자가 아닙니다</span>}
-      <button onClick={onClickSwitch}>전환</button>
-      <Card />
-    </div>
+    <AdminFlagContext.Provider value={{ isAdmin, setIsAdmin }}>
+      <div>
+        {isAdmin ? <span>관리자입니다</span> : <span>관리자가 아닙니다</span>}
+        <button onClick={onClickSwitch}>전환</button>
+        <Card />
+      </div>
+    </AdminFlagContext.Provider>
   );
 };
