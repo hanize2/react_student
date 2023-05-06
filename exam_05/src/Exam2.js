809c0b7e7b38149481dde0720f3b56b589892030
JinWooJung <hanize2@gmail.com>
2023-05-06 21:27:28 +0900

    5월 수행평가 1번문제 수정

---
+++ exam_05/src/Exam2.js
@@ -0,0 +1,13 @@
+import EmailInputComponents from "./components/EmailInputComponents";
+import PwdInputComponents from "./components/PwdInputComponents";
+import exam1Gif from "./exam1.gif";
+
+const Exam1 = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
+
+export default Exam1;
--- exam_05/src/index.js
+++ exam_05/src/index.js
@@ -6,10 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 import Exam1 from "./Exam1";
+import Exam2 from "./Exam2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Exam1 />
+    <Exam2 />
   </React.StrictMode>
 );
