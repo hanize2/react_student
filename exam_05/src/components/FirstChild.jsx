dd51e8d8f8e897fbfa3d308bee5ea329f99c807c
JinWooJung <hanize2@gmail.com>
2023-05-07 03:45:25 +0900

    5월 수행평가 2번문제

---
+++ exam_05/src/components/FirstChild.jsx
@@ -0,0 +1,17 @@
+import SecondChild from "./SecondChild";
+
+const FirstChild = () => {
+  console.log("FirstChild가 렌더링되었습니다.");
+
+  return (
+    <div className="card bg-warning btn-sm text-white">
+      <div className="card-body">
+        <h5 className="card-title">자식 컴포넌트1</h5>
+        <button className="w-100 btn btn-success">0으로 초기화</button>
+        <SecondChild />
+      </div>
+    </div>
+  );
+};
+
+export default FirstChild;
